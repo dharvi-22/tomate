4fc211f7a5f498953bf2fce399c8f5fce5829fea
Dharvi <dharvijit.dj@gmail.com>
2025-03-19 21:48:08 +0000

    fix: started header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/main.scss';
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
       <header>
+        <Header />
         <h1>
           Home :)
         </h1>
--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,18 @@
 import React from "react";
+import "./header.scss"; 
+
+const Header =() => {
+    return (
+        <header className ="header">
+            <ul className = "nav-list">
+                <li>
+                    <a href="#"> MEAL MATCH </a> 
+                </li>
+                <li>
+                    <a href="#"> EXPLORE </a> 
+                </li>
+            </ul>
+        </header>
+    );
+};
+export default Header 
