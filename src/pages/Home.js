09b32857067ec805e0a4b2e282f1e0afb212ce4a
Dharvi <dharvijit.dj@gmail.com>
2025-03-30 22:57:56 +0100

    feat: started home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,89 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Icon} from "../assets/t-icon.svg";
+import background from "../assets/background.svg";
+import pastaImg from "../assets/pasta.png";
+import pizzaImg from "../assets/pizza.png";
+import chiliIcon from "../assets/chili.svg";
+import pepper from "../assets/pepper.svg";
+import peasnap from "../assets/peasnap.svg";
+import brocoli from "../assets/brocoli.svg";
+import onion from "../assets/onion.svg";
+import tomatoo from "../assets/tomatooo.svg";
+import mushroom from "../assets/mushroom.svg";
+
+import "../styles/home.scss";
 
 const Home = () => {
   return(
-   <h1>Welcome to Home:</h1>
-  )
+    <>
+    {/* search by category card*/}
+    <div className="hero-container">
+        <img src={background} alt="circular-background" className="hero-background"/>
+        <Link to="/explore" className="home-button">Explore Recipes</Link>
+        <h1>Quick, Easy & <br />
+          <span>Perfectly Portioned for One.</span>
+        </h1>
+        <p>Find tasty recipes that suite your portion size.</p>
+
+      {/* meal match card*/}
+      <div className="hero-cards">
+        <div className="hero-card">
+          <img src={pizzaImg} alt="pizza" />
+            <h3>Meal Match</h3>
+            <p>Find tasty recipes that suite your portion size.</p>
+        </div>
+      
+      {/* search by category card*/}
+      <div className="hero-card">
+          <img src={pastaImg} alt="pasta" />
+            <h3>Search by Category</h3>
+            <p>Find tasty recipes that suite your portion size.</p>
+      </div>
+
+      <div className="hero-icon"> 
+        <Icon/>
+      </div>
+    </div>
+    </div>
+   
+    <div className="carousel-container">
+      <div className="carousel-track">
+          <img src={chiliIcon} alt="chili"/>
+          <img src={mushroom} alt="mushroom"/>
+          <img src={pepper} alt="pepper"/>
+          <img src={peasnap} alt="pea-snap"/>
+          <img src={brocoli} alt="brocoli"/>
+          <img src={tomatoo} alt="tomatoes on vine"/>
+          <img src={onion} alt="onion"/>
+
+          <img src={chiliIcon} alt="chili"/>
+          <img src={mushroom} alt="mushroom"/>
+          <img src={pepper} alt="pepper"/>
+          <img src={peasnap} alt="pea-snap"/>
+          <img src={brocoli} alt="brocoli"/>
+          <img src={tomatoo} alt="tomatoes on vine"/>
+          <img src={onion} alt="onion"/>
+
+          <img src={chiliIcon} alt="chili"/>
+          <img src={mushroom} alt="mushroom"/>
+          <img src={pepper} alt="pepper"/>
+          <img src={peasnap} alt="pea-snap"/>
+          <img src={brocoli} alt="brocoli"/>
+          <img src={tomatoo} alt="tomatoes on vine"/>
+          <img src={onion} alt="onion"/>
+
+          <img src={chiliIcon} alt="chili"/>
+          <img src={mushroom} alt="mushroom"/>
+          <img src={pepper} alt="pepper"/>
+          <img src={peasnap} alt="pea-snap"/>
+          <img src={brocoli} alt="brocoli"/>
+          <img src={tomatoo} alt="tomatoes on vine"/>
+          <img src={onion} alt="onion"/>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
