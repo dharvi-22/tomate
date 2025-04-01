979c2e8c310298efb957ffa69858e9c599ca3c5c
Dharvi <dharvijit.dj@gmail.com>
2025-04-01 13:52:06 +0100

    feat: started meal match page

--- src/pages/MealMatch.js
+++ src/pages/MealMatch.js
@@ -1,8 +1,13 @@
 import React from "react";
+import "../styles/mealMatch.scss";
 
 const MealMatch = () => {
     return (
-    <h1>Welcome to Meal-Match:</h1>
+    <div className="match-container">
+        <h1>Meal Match</h1>
+        <p>Browse quickly through our recipes catalogue to find the meal that matches your taste buds and mood! Simply swipe left for more or right to view the recipe in more detail.</p>
+        <button className="red-button">Play Meal Match</button>
+    </div>
     )
 };
   
